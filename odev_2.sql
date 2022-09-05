2ffb34c6f1e0bee974bb62c2df524622f1afb924
fsoysall <fsoysal@gmail.com>
2022-09-06 01:43:51 +0300

    ödev 2 -1

--- odev_2.sql
+++ odev_2.sql
@@ -1,1 +1,7 @@
-SELECT * from film where replacement_cost >= 12.99 <nd replacement_cost < 16.99
+SELECT * from film where replacement_cost >= 12.99 and replacement_cost < 16.99;
+SELECT * from film where replacement_cost BETWEEN  value >= 12.99 and  16.99;
+
+
+select * from actor where first_name in ( 'Penelope' , 'Nick' , 'Ed' ) order by 1;
+
+select * from film rental_rate in (0.99, 2.99, 4.99) and  replacement_cost (12.99, 15.99, 28.99 );
